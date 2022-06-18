580e632ca8e7c003fa6e38771244a4b2c20ce650
Samiul Basir <samiulbasirbhuiyan1234@gmail.com>
2022-06-18 16:45:13 +0600

    6/8/22

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 
 import { createRoot } from 'react-dom/client';
 import App from "./App"
-import "./index.js"
+import "./index.css"
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App />, root);
